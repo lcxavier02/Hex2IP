b192355830959065cc187e688df8287667e78462
Xavier Velazquez <luisvelazquez98271@gmail.com>
2024-02-22 21:20:11 -0600

    Added new functions to convert

---
+++ src/hex2ip/Hex2IP.java
@@ -0,0 +1,59 @@
+package hex2ip;
+
+public class Hex2IP {
+    
+    public void convertIpToHex(String ip) {
+        
+    }
+    
+    public void convertHexToIp(String hex) {
+        int oct = hex.length() / 2;
+        String[] octPairs = new String[oct];
+        int[] decPairs = new int[oct];
+        
+        if (hex.length() < 8) {
+            System.out.println("Ingresa un cadena hexadecimal valida (8 caracteres).");
+        }
+        
+        for (int i = 0; i < oct; i++) {
+            String pair = hex.substring(2 * i, 2 * i + 2);
+            if (!pair.matches("[0-9A-Fa-f]{2}")) {
+                System.out.println("La cadena hexadecimal contiene caracteres no válidos.");
+                return;
+            }
+            octPairs[i] = pair;
+        }
+        
+        for(int i = 0; i < oct; i++) {
+            decPairs[i] = Integer.parseInt(octPairs[i], 16);
+        }
+        
+        String ip = String.join(".", Integer.toString(decPairs[0]), Integer.toString(decPairs[1]), Integer.toString(decPairs[2]), Integer.toString(decPairs[3]));
+        
+        System.out.println("El dexadecimal: " + hex + " a direccion IP es: " + ip);
+    }
+    
+    public static void main(String[] args) {
+        boolean isParameterMissing;
+        Hex2IP htip = new Hex2IP();
+        
+        switch (args.length) {
+            case 0 -> {
+                System.out.println("No se proporcionaron argumentos.");
+                return;
+            }
+            case 1 -> isParameterMissing = true;
+            default -> isParameterMissing = false;
+        }
+        
+        if (args[0].equals("-hex") && !isParameterMissing) {
+            htip.convertHexToIp(args[1]);
+        }
+        else if (args[0].equals("-ip") && !isParameterMissing){
+            htip.convertIpToHex(args[1]);
+        }
+        else if (isParameterMissing){
+            System.out.println("Segundo parametro vacio no permitido.");
+        }
+    }
+}
